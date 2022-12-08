8167fb098d66ed632061e5fd5af291f5b450a07b
Cristian Caiza <cristiancaiza@hotmail.com>
2022-12-07 21:26:08 -0500

    Arreglando diseño

--- index.php
+++ index.php
@@ -132,56 +132,6 @@ $estudiante = "SELECT * FROM tbl_estudiante";
         </tbody>
     </table>
 
-
-    <div class="container-table">
-
-        <div class="table__header">Cedula</div>
-        <div class="table__header">Nombres</div>
-        <div class="table__header">Apellidos</div>
-        <div class="table__header">Fecha de Nacimiento</div>
-        <div class="table__header">Telefono</div>
-        <div class="table__header">Correo</div>
-        <div class="table__header">Representante del Alumno</div>
-        <div class="table__header">Direccion</div>
-
-        <?php
-        $resultado = mysqli_query($conexion, $estudiante);
-
-        while ($row = mysqli_fetch_assoc($resultado)) {
-        ?>
-
-        <div class="table__item">
-            <?php echo $row["estCedula"]; ?>
-        </div>
-        <div class="table__item">
-            <?php echo $row["estNombres"]; ?>
-        </div>
-        <div class="table__item">
-            <?php echo $row["estApellidos"]; ?>
-        </div>
-        <div class="table__item">
-            <?php echo $row["estFechaNacimiento"]; ?>
-        </div>
-        <div class="table__item">
-            <?php echo $row["estTelefono"]; ?>
-        </div>
-        <div class="table__item">
-            <?php echo $row["estCorreo"]; ?>
-        </div>
-        <div class="table__item">
-            <?php echo $row["estRepresentantePadres"]; ?>
-        </div>
-        <div class="table__item">
-            <?php echo $row["estDireccion"]; ?>
-        </div>
-
-        <?php
-        }
-        ?>
-
-
-
-
 </body>
 
 </html>
