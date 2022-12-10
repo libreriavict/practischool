b0d9520f10ef2bd94ba31793e96dcfad7bf11a2c
Cristian Caiza <cristiancaiza@hotmail.com>
2022-12-09 19:15:46 -0500

    Agregando el tr

--- index.php
+++ index.php
@@ -107,6 +107,7 @@ $estudiante = "SELECT * FROM tbl_estudiante";
                             <a href="actualizar.php?id=<?php echo $row['idEstudiante']?>" class="btn btn-info">Editar</a>
                             <a href="eliminar.php?id=<?php echo $row['idEstudiante']?>" class="btn btn-danger">Eliminar</a>
                         </td>
+                        <tr>
                     <?php
                     }
                     ?>
